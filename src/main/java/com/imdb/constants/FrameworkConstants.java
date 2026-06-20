eac923983021e819fca821e2ce767022ee7c9e3a
Ranjini-V <ranjini.v.iyer35@gmail.com>
2026-06-20 21:48:30 +0530

    Added FrameworkConstants class

---
+++ src/main/java/com/imdb/constants/FrameworkConstants.java
@@ -0,0 +1,11 @@
+package com.imdb.constants;
+
+public final class FrameworkConstants {
+
+    private FrameworkConstants() {
+
+    }
+
+    public static final String PROJECT_PATH = System.getProperty("user.dir");
+
+}
